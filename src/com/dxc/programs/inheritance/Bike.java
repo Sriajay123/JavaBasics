2ffcef5092dba5a596cee2dc70c023e4f1cc5971
sajays <sriajaysanipini047@gmail.com>
2022-02-08 19:27:53 +0530

    new commit

--- src/com/dxc/programs/inheritance/Bike.java
+++ src/com/dxc/programs/inheritance/Bike.java
@@ -1,49 +1,43 @@
 package com.dxc.programs.inheritance;
-
+//Single level
 class Bike {
 
-    public Bike()
-    {
-        System.out.println("hii");
+    public Bike() {
+        System.out.println("two wheeler");
     }
 
-
-
-}
-
-//Single level
-
-class Yamaha extends Bike {
-    public void yamaha()
-    {
-        System.out.println("Yamaha");
-    }
-
-}
-//multilevel
-
-class RX extends Yamaha{
-    public void rx()
-    {
-        System.out.println("RX");
-    }
-}
-
-// Hierarchical
-
-class Hero extends Bike {
-    public void hero()
-    {
-        System.out.println("Hero");
+    public void display() {
+        System.out.println("bike ");
     }
 
 }
 
-class Test{
+    class Yamaha extends Bike {
+
+        public Yamaha()
+        {
+            super();
+            System.out.println("started in 1990");
+        }
+        public void display1() {
+            System.out.println("Yamaha");
+        }
+
+
+
+    }
+
+ class Demo{
     public static void main(String[] args) {
-        Bike  b=new Bike();
-       b.drive();
 
+        Yamaha y=new Yamaha();
+        y.display();
+        y.display1();
     }
 }
+
+
+
+
+
 
